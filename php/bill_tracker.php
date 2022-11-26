438e461baa0696604ae1c3fab39c230de45171f5
EngineerYo <willcrain96@gmail.com>
2022-11-25 18:46:12 -0800

    Added switch

--- php/bill_tracker.php
+++ php/bill_tracker.php
@@ -1,10 +1,25 @@
-<!DOCTYPE html>
 <html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="../css/bill_tracker.css" rel="stylesheet" type="text/css" />
+	</head>
 	<body>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+		<script type="module" src="../js/bill_tracker.js"></script>
 
-		<?php
-		echo "Hello World!";
-		?>
+		<div class="container">
+			<div class="x background"></div>
+			<div id="switch">
+				<div id="screw"></div>
+				<button class="x switcher">
+					<div class="x switchBg" id="sarah"></div>
+					<div class="x switchBg" id="will"></div>
+					<div class="x switchHalf" id="sarah">SARAH</div>
+					<div class="x switchHalf" id="will">WILL</div>
+				</button>
+				<div id="screw"></div>
+			</div>
+		</div>
 
 	</body>
 </html>
